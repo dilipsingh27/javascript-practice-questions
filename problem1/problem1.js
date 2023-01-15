4a485dcf28a68a2b82d0b7cd446426be56ee337e
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 00:35:14 +0530

    feat: add to find largest even number

---
+++ problem1/problem1.js
@@ -0,0 +1,53 @@
+// function largestEven(arr) {
+//     let max = -1;
+//     for(let i=0;i<arr.length;i++) {
+//         if((arr[i]%2)===0)
+//         {   
+//             if(arr[i]>max)
+//             max = arr[i];
+//         }
+//     }
+//     if(max !== -1)
+//     console.log(max)
+//     else
+//     console.log("Even number doesn't exist")
+// }
+
+// largestEven([1,2,3,4,5])
+
+ const isEven = (number) => number%2===0
+
+// function largestEven(numbers) {
+//     let largestEven = -1;
+//     numbers.forEach(number => {
+//         switch(isEven(number)) {
+//             case true: largestEven=Math.max(largestEven,number)
+//             break;
+//         }
+//     });
+
+//     if(largestEven !== -1)
+//     console.log(largestEven)
+//     else
+//     console.log("Even number doesn't exist")
+
+// }
+
+
+const largestEven = (numbers) => {
+    let largestEven = -1;
+    numbers.forEach(number => {
+        switch(isEven(number)) {
+            case true: largestEven=Math.max(largestEven,number)
+            break;
+        }
+    });
+
+    if(largestEven !== -1)
+    console.log(largestEven)
+    else
+    console.log("Even number doesn't exist")
+
+}
+
+largestEven([1,2,3,4,5])

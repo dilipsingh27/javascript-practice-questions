8513acf6827d91c0d263f1af9257a4f63c93f32d
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 00:44:39 +0530

    feat: add to count vowel in a string

---
+++ problem8/problem8.js
@@ -0,0 +1,20 @@
+// function countVowel(word) {
+//     let count = 0;
+//     const regex = /[a,e,i,o,u,A,E,I,O,U]/
+//     word.split('').forEach((char) => {
+//         regex.test(char)?count+=1:null
+//     })
+//     return count
+// }
+
+
+const countVowel = (word) => {
+    let count = 0;
+    const regex = /[a,e,i,o,u,A,E,I,O,U]/
+    word.split('').forEach((char) => {
+        regex.test(char)?count+=1:null
+    })
+    return count
+}
+console.log(countVowel("codeacademy"))
+console.log(countVowel("dilipSIngh"))

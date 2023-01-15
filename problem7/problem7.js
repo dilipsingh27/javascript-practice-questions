e308c6c3e18fd92a1ac5d860f70238d68e14ec7b
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 00:43:57 +0530

    feat: add to concatenate 2 string except starting letter

---
+++ problem7/problem7.js
@@ -0,0 +1,7 @@
+// function concatenateTwoString(arrays) {
+//     return arrays[0].slice(1) + arrays[1].slice(1)
+// }
+
+const concatenateTwoString = (arrays) => arrays[0].slice(1) + arrays[1].slice(1)
+
+console.log(concatenateTwoString(['code', 'academy']))

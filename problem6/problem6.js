3bdfd3a8d110c09a681fbb6236e1316a083085a0
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 02:18:15 +0530

    fix: bugs reltd to testcase file(module.exports)

--- problem6/problem6.js
+++ problem6/problem6.js
@@ -1,12 +1,12 @@
-// function isEndingWithWordScript(word) {
-//     let wordLength = word.length
-//     let scriptIndex = word.length - 6;
-//     if(scriptIndex<=0) return false
-//     return word.slice(scriptIndex) === 'Script'
-// }
+function isEndingWithWordScript(word) {
+    let wordLength = word.length
+    let scriptIndex = word.length - 6;
+    if(scriptIndex<=0) return false
+    return word.slice(scriptIndex) === 'Script'
+}
 
 
-const isEndingWithWordScript = (word) => {
+const isEndingWithWordScriptArrow = (word) => {
     let wordLength = word.length
     let scriptIndex = word.length - 6;
     if(scriptIndex<=0) return false
@@ -15,3 +15,8 @@ const isEndingWithWordScript = (word) => {
 
 console.log(isEndingWithWordScript('javaScript'))
 console.log(isEndingWithWordScript('javascript'))
+
+module.exports = {
+    isEndingWithWordScript:isEndingWithWordScript,
+    isEndingWithWordScriptArrow:isEndingWithWordScriptArrow
+}

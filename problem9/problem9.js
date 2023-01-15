cb318533d8fec395fddc38ddbe9ca0a529f57c74
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 02:39:54 +0530

    fix: bugs reltd to testcases

--- problem9/problem9.js
+++ problem9/problem9.js
@@ -1,18 +1,18 @@
-// function replaceCharacter(word) {
-//     let newWord=''
-//     word.split('').forEach(char => {
-//         let unicode = char.charCodeAt()
-//         unicode===90?unicode=64:null
-//         unicode===122?unicode=96:null
-//         let replacedChar = String.fromCharCode(unicode+1)
-//         newWord+=replacedChar
+function replaceCharacter(word) {
+    let newWord=''
+    word.split('').forEach(char => {
+        let unicode = char.charCodeAt()
+        unicode===90?unicode=64:null
+        unicode===122?unicode=96:null
+        let replacedChar = String.fromCharCode(unicode+1)
+        newWord+=replacedChar
 
-//     })
-//     return newWord
-// }
+    })
+    return newWord
+}
 
 
-const replaceCharacter = (word) => {
+const replaceCharacterArrow = (word) => {
     let newWord=''
     word.split('').forEach(char => {
         let unicode = char.charCodeAt()
@@ -26,3 +26,10 @@ const replaceCharacter = (word) => {
 }
 
 console.log(replaceCharacter('codeacademy'))
+console.log(replaceCharacter('dilipSIngh'))
+
+
+module.exports = {
+    replaceCharacter:replaceCharacter,
+    replaceCharacterArrow:replaceCharacterArrow
+}

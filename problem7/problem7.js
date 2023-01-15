17d17d1a6cff2dc9c0bafd04c172677b07f6c21f
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 02:33:31 +0530

    fix: bugs reltd with testcase file

--- problem7/problem7.js
+++ problem7/problem7.js
@@ -1,7 +1,12 @@
-// function concatenateTwoString(arrays) {
-//     return arrays[0].slice(1) + arrays[1].slice(1)
-// }
+function concatenateTwoString(arrays) {
+    return arrays[0].slice(1) + arrays[1].slice(1)
+}
 
-const concatenateTwoString = (arrays) => arrays[0].slice(1) + arrays[1].slice(1)
+const concatenateTwoStringArrow = (arrays) => arrays[0].slice(1) + arrays[1].slice(1)
 
-console.log(concatenateTwoString(['code', 'academy']))
+// console.log(concatenateTwoString(['code', 'academy']))
+
+module.exports = {
+    concatenateTwoString:concatenateTwoString,
+    concatenateTwoStringArrow:concatenateTwoStringArrow
+}

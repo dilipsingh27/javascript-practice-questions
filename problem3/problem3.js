c7338b781a07e370a1e0d4b28c77f78256418ca4
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 01:50:31 +0530

    fix: bugs for testing environment

--- problem3/problem3.js
+++ problem3/problem3.js
@@ -1,16 +1,4 @@
-// function convert(sentence) {
-//     // to split the string into array of substring
-//     let myArr = sentence.split('')
-//     let SENTENCE = ''
-//     const regex = /[A-Z]/
-//     myArr.forEach(char => {
-//         regex.test(char)?SENTENCE+=char.toLowerCase():SENTENCE+=char.toUpperCase()
-//     })
-//     return SENTENCE
-// }
-
-
-const convert = (sentence) => {
+function convert(sentence) {
     // to split the string into array of substring
     let myArr = sentence.split('')
     let SENTENCE = ''
@@ -21,4 +9,24 @@ const convert = (sentence) => {
     return SENTENCE
 }
 
-console.log(convert("MeRRy hAD a LITTle lAMp"))
+
+const convertArrow = (sentence) => {
+    // to split the string into array of substring
+    let myArr = sentence.split('')
+    let SENTENCE = ''
+    const regex = /[A-Z]/
+    myArr.forEach(char => {
+        regex.test(char)?SENTENCE+=char.toLowerCase():SENTENCE+=char.toUpperCase()
+    })
+    return SENTENCE
+}
+
+// console.log(convert("MeRRy hAD a LITTle lAMp"))
+// console.log(convert("My NAme Is DiLIp"))
+
+
+
+module.exports = {
+    convert:convert,
+    convertArrow:convertArrow
+}

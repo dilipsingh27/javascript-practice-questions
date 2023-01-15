6d5e590d179aa1ba75d3d071ed3951ffa543c5c4
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-01-16 02:08:14 +0530

    fix: bugs reltd with testcase file

--- problem5/problem5.js
+++ problem5/problem5.js
@@ -1,11 +1,16 @@
-// function isLeapYearOrNot(year) {
-//     return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
+function isLeapYearOrNot(year) {
+    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
 
-// }
+}
 
-const isLeapYearOrNot = (year) => (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
+const isLeapYearOrNotArrow = (year) => (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
 
 
 
-console.log(isLeapYearOrNot(2000))
-console.log(isLeapYearOrNot(1999))
+// console.log(isLeapYearOrNot(2000))
+// console.log(isLeapYearOrNot(1999))
+
+module.exports = {
+    isLeapYearOrNot:isLeapYearOrNot,
+    isLeapYearOrNotArrow:isLeapYearOrNotArrow
+}
